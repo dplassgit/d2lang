566c513f90e1a55abacb6bbd3fe55b8c3e8c864a
David Plass <dplassgit@users.noreply.github.com>
2021-05-04 21:04:30 -0400

    Primitive code generation for boolean and unary operators

--- src/com/plasstech/lang/d2/codegen/CodeGenerator.java
+++ src/com/plasstech/lang/d2/codegen/CodeGenerator.java
@@ -3,10 +3,12 @@ package com.plasstech.lang.d2.codegen;
 import com.plasstech.lang.d2.common.DefaultVisitor;
 import com.plasstech.lang.d2.parse.AssignmentNode;
 import com.plasstech.lang.d2.parse.BinOpNode;
+import com.plasstech.lang.d2.parse.BoolNode;
 import com.plasstech.lang.d2.parse.IntNode;
 import com.plasstech.lang.d2.parse.Node;
 import com.plasstech.lang.d2.parse.PrintNode;
 import com.plasstech.lang.d2.parse.StatementsNode;
+import com.plasstech.lang.d2.parse.UnaryNode;
 import com.plasstech.lang.d2.parse.VariableNode;
 
 public class CodeGenerator extends DefaultVisitor {
@@ -43,6 +45,13 @@ public class CodeGenerator extends DefaultVisitor {
     System.out.printf("; %s\n", node);
     // Provide constant in r0
     emit(String.format("ld r0, #%s", node.value()));
+  }
+
+  @Override
+  public void visit(BoolNode node) {
+    System.out.printf("; %s\n", node);
+    // Provide constant in r0
+    emit(String.format("ld r0, #%d", node.value() ? 1 : 0));
   }
 
   @Override
@@ -87,6 +96,27 @@ public class CodeGenerator extends DefaultVisitor {
     }
   }
 
+  @Override
+  public void visit(UnaryNode node) {
+    System.out.printf("; %s\n", node);
+    // calculate the value and set it in r0
+    Node expr = node.expr();
+    expr.accept(this);
+
+    switch (node.operator()) {
+      case NOT:
+      case MINUS: // want r0 to be 0-r1
+        emit("ld r1, r0");
+        emit("ld r0, #0");
+        emit("sbc");
+        emit("sub r0, r1\n");
+        break;
+      case PLUS:
+        break;
+      default:
+        break;
+    }
+  }
   private void emit(String string) {
     System.out.println(string);
   }
