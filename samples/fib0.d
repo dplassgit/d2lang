d683484fc03c7331b4fdcf05c23aa46028911f6c
David Plass <dplassgit@users.noreply.github.com>
2021-05-28 15:27:44 -0400

    Updated fib sample to have better output

--- samples/fib0.d
+++ samples/fib0.d
@@ -1,5 +1,5 @@
 
-n=10
+n=50
 n1 = 0
 n2 = 1
 i=1 while i < n *2 do i = i+1 {
@@ -9,6 +9,10 @@ i=1 while i < n *2 do i = i+1 {
   nth = n1 + n2
   n1 = n2
   n2 = nth
-  print nth
+  print i/2 
+  print " th fib: " // NOTE: starting the string with a t screws up the Interpreter... 
+  println nth
 }
-print nth
+println '' // NOTE: cannot just do PRINT (no expression)...
+print "Final fib: "
+println nth
