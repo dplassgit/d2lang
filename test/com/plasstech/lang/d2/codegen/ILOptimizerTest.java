0769abb1410e94d74705e0080e0e4bd7176bcdf6
David Plass <dplassgit@users.noreply.github.com>
2021-06-18 19:01:08 -0400

    Refactored tests. Test all files in the samples/ directory (only within Eclipse)

--- test/com/plasstech/lang/d2/codegen/ILOptimizerTest.java
+++ test/com/plasstech/lang/d2/codegen/ILOptimizerTest.java
@@ -1,46 +1,33 @@
 package com.plasstech.lang.d2.codegen;
 
-import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import static org.junit.Assert.fail;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.common.base.Joiner;
 import com.plasstech.lang.d2.ExecutionEnvironment;
 import com.plasstech.lang.d2.codegen.il.Op;
-import com.plasstech.lang.d2.interpreter.Environment;
 
 public class ILOptimizerTest {
 
   @Test
   public void plusZero() {
-    optimizeAssertSameVariables("a = 0 + 1 b = a + 0 c = 2 + 0");
+    TestUtils.optimizeAssertSameVariables("a = 0 + 1 b = a + 0 c = 2 + 0");
   }
 
   @Test
   public void plusConstants() {
-    optimizeAssertSameVariables("a = 2 + 3");
+    TestUtils.optimizeAssertSameVariables("a = 2 + 3");
   }
 
   @Test
   public void minusConstants() {
-    optimizeAssertSameVariables("a = 2 - 3");
+    TestUtils.optimizeAssertSameVariables("a = 2 - 3");
   }
 
   @Test
   public void divConstants() {
-    optimizeAssertSameVariables("a = 10 / 2");
+    TestUtils.optimizeAssertSameVariables("a = 10 / 2");
   }
 
   @Test
@@ -63,124 +50,124 @@ public class ILOptimizerTest {
 
   @Test
   public void modConstants() {
-    optimizeAssertSameVariables("a = 14 % 5");
+    TestUtils.optimizeAssertSameVariables("a = 14 % 5");
   }
 
   @Test
   public void plusStringConstants() {
-    optimizeAssertSameVariables("a = 'hi' + ' there'");
+    TestUtils.optimizeAssertSameVariables("a = 'hi' + ' there'");
   }
 
   @Test
   public void timesZero() {
-    optimizeAssertSameVariables("a = 1 * 0 b = a * 0 d = 3 * 0");
+    TestUtils.optimizeAssertSameVariables("a = 1 * 0 b = a * 0 d = 3 * 0");
   }
 
   @Test
   public void timesConstants() {
-    optimizeAssertSameVariables("a = 2 * 3");
+    TestUtils.optimizeAssertSameVariables("a = 2 * 3");
   }
 
   @Test
   public void andConstant() {
-    optimizeAssertSameVariables("a = true b = a & true");
-    optimizeAssertSameVariables("a = true b = true and a");
-    optimizeAssertSameVariables("a = true and true");
-    optimizeAssertSameVariables("a = true & false");
-    optimizeAssertSameVariables("a = false and true");
-    optimizeAssertSameVariables("a = false and false");
+    TestUtils.optimizeAssertSameVariables("a = true b = a & true");
+    TestUtils.optimizeAssertSameVariables("a = true b = true and a");
+    TestUtils.optimizeAssertSameVariables("a = true and true");
+    TestUtils.optimizeAssertSameVariables("a = true & false");
+    TestUtils.optimizeAssertSameVariables("a = false and true");
+    TestUtils.optimizeAssertSameVariables("a = false and false");
   }
 
   @Test
   public void orConstant() {
-    optimizeAssertSameVariables("a = true b = a | true");
-    optimizeAssertSameVariables("a = true b = true or a");
-    optimizeAssertSameVariables("a = true or true");
-    optimizeAssertSameVariables("a = true or false");
-    optimizeAssertSameVariables("a = false | true");
-    optimizeAssertSameVariables("a = false or false");
+    TestUtils.optimizeAssertSameVariables("a = true b = a | true");
+    TestUtils.optimizeAssertSameVariables("a = true b = true or a");
+    TestUtils.optimizeAssertSameVariables("a = true or true");
+    TestUtils.optimizeAssertSameVariables("a = true or false");
+    TestUtils.optimizeAssertSameVariables("a = false | true");
+    TestUtils.optimizeAssertSameVariables("a = false or false");
   }
 
   @Test
   public void eqIntConstant() {
-    optimizeAssertSameVariables("a = 3==3");
-    optimizeAssertSameVariables("a = 4==3");
-    optimizeAssertSameVariables("a = 3!=3");
-    optimizeAssertSameVariables("a = 4!=3");
+    TestUtils.optimizeAssertSameVariables("a = 3==3");
+    TestUtils.optimizeAssertSameVariables("a = 4==3");
+    TestUtils.optimizeAssertSameVariables("a = 3!=3");
+    TestUtils.optimizeAssertSameVariables("a = 4!=3");
   }
 
   @Test
   public void eqStringConstant() {
-    optimizeAssertSameVariables("a = 'hi' == 'bye'");
-    optimizeAssertSameVariables("a = 'hi' == 'hi'");
-    optimizeAssertSameVariables("a = 'hi' != 'bye'");
-    optimizeAssertSameVariables("a = 'hi' != 'hi'");
+    TestUtils.optimizeAssertSameVariables("a = 'hi' == 'bye'");
+    TestUtils.optimizeAssertSameVariables("a = 'hi' == 'hi'");
+    TestUtils.optimizeAssertSameVariables("a = 'hi' != 'bye'");
+    TestUtils.optimizeAssertSameVariables("a = 'hi' != 'hi'");
   }
 
   @Test
   public void eqBoolConstant() {
-    optimizeAssertSameVariables("a = true == true");
-    optimizeAssertSameVariables("a = true != true");
-    optimizeAssertSameVariables("a = true == false");
-    optimizeAssertSameVariables("a = true != false");
-    optimizeAssertSameVariables("a = false == false");
-    optimizeAssertSameVariables("a = false != false");
+    TestUtils.optimizeAssertSameVariables("a = true == true");
+    TestUtils.optimizeAssertSameVariables("a = true != true");
+    TestUtils.optimizeAssertSameVariables("a = true == false");
+    TestUtils.optimizeAssertSameVariables("a = true != false");
+    TestUtils.optimizeAssertSameVariables("a = false == false");
+    TestUtils.optimizeAssertSameVariables("a = false != false");
   }
 
   @Test
   public void ltGtIntConstant() {
-    optimizeAssertSameVariables("a = 3>3");
-    optimizeAssertSameVariables("a = 4>=3");
-    optimizeAssertSameVariables("a = 3<3");
-    optimizeAssertSameVariables("a = 4<=3");
+    TestUtils.optimizeAssertSameVariables("a = 3>3");
+    TestUtils.optimizeAssertSameVariables("a = 4>=3");
+    TestUtils.optimizeAssertSameVariables("a = 3<3");
+    TestUtils.optimizeAssertSameVariables("a = 4<=3");
   }
 
   @Test
   public void constantPropagationTransfer() {
-    optimizeAssertSameVariables("a = 4 b = a");
+    TestUtils.optimizeAssertSameVariables("a = 4 b = a");
   }
 
   @Test
   public void constantAsc() {
-    optimizeAssertSameVariables("a = asc('b') b = a");
+    TestUtils.optimizeAssertSameVariables("a = asc('b') b = a");
   }
 
   @Test
   public void constantChr() {
-    optimizeAssertSameVariables("a = chr(65) b = a");
+    TestUtils.optimizeAssertSameVariables("a = chr(65) b = a");
   }
 
   @Test
   public void constantStringLength() {
-    optimizeAssertSameVariables("a = length('abc') b = a");
+    TestUtils.optimizeAssertSameVariables("a = length('abc') b = a");
   }
 
   @Test
   public void constantArrayLength() {
-    optimizeAssertSameVariables("a = length([1,2,3,4]) b = a");
-    optimizeAssertSameVariables("a = length([true, false]) b = a");
-    optimizeAssertSameVariables("a = length(['a', 'b', 'c']) b = a");
+    TestUtils.optimizeAssertSameVariables("a = length([1,2,3,4]) b = a");
+    TestUtils.optimizeAssertSameVariables("a = length([true, false]) b = a");
+    TestUtils.optimizeAssertSameVariables("a = length(['a', 'b', 'c']) b = a");
   }
 
   @Test
   public void constantPropIf() {
-    optimizeAssertSameVariables("a = 4 if a ==3 { print a}");
+    TestUtils.optimizeAssertSameVariables("a = 4 if a ==3 { print a}");
   }
 
   @Test
   public void constantPropReturn() {
-    optimizeAssertSameVariables("a:proc():int { return 3} print a()");
+    TestUtils.optimizeAssertSameVariables("a:proc():int { return 3} print a()");
   }
 
   @Test
   public void constantPropCall() {
-    optimizeAssertSameVariables(
+    TestUtils.optimizeAssertSameVariables(
         "a:proc(n:int, m:int):int { return n+1} b=4 print a(4, b) print a(b+2, 4+6)");
   }
 
   @Test
   public void multipleReturns() {
-    optimizeAssertSameVariables(
+    TestUtils.optimizeAssertSameVariables(
         "toString: proc(i: int): string {\r\n"
             + "  if i == 0 {\r\n"
             + "    return '0'\r\n"
@@ -189,57 +176,5 @@ public class ILOptimizerTest {
             + "  return val\r\n"
             + "}"
             + "  println toString(314159)\r\n");
-  }
-
-  @Test
-  @Ignore("Very slow")
-  public void lexerInD() {
-    String path = "samples/d2ind2/lexerglobals.d";
-    testFromFile(path);
-  }
-
-  @Test
-  public void testAllSamples() throws Exception {
-    List<File> files =
-        Files.list(Paths.get("samples"))
-            .filter(Files::isRegularFile)
-            .filter(path -> path.toString().endsWith(".d"))
-            .map(Path::toFile)
-            .collect(Collectors.toList());
-    for (File file : files) {
-      testFromFile(file.getAbsolutePath());
-    }
-  }
-
-  private void testFromFile(String path) {
-    try {
-      String text = new String(Files.readAllBytes(Paths.get(path)));
-      optimizeAssertSameVariables(text);
-    } catch (IOException e) {
-      e.printStackTrace();
-      fail("Could not read " + path);
-    } catch (RuntimeException e) {
-      e.printStackTrace();
-      fail("Could not compile " + path + " " + e.getMessage());
-    }
-  }
-
-  private void optimizeAssertSameVariables(String program) {
-    ExecutionEnvironment ee = new ExecutionEnvironment(program);
-    Environment env = ee.execute();
-    System.out.println(Joiner.on("\n").join(ee.ilCode()));
-
-    List<Op> originalCode = new ArrayList<>(ee.ilCode());
-
-    List<Op> optimized = new ILOptimizer().optimize(ee.ilCode());
-    System.out.println("\nOPTIMIZED:");
-    System.out.println(Joiner.on("\n").join(optimized));
-    Environment env2 = ee.execute(optimized);
-    assertThat(env2.variables()).isEqualTo(env.variables());
-    assertThat(env2.output()).isEqualTo(env.output());
-
-    assertWithMessage("Should have made at least one optimization")
-        .that(originalCode)
-        .isNotEqualTo(optimized);
   }
 }
