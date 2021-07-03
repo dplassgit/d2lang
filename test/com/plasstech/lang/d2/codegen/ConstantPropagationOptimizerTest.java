92849260418b31da5cda47e5527a4517bec8d07a
David Plass <dplassgit@users.noreply.github.com>
2021-07-03 19:59:12 -0400

    Clean up constant propagation optimizer test

--- test/com/plasstech/lang/d2/codegen/ConstantPropagationOptimizerTest.java
+++ test/com/plasstech/lang/d2/codegen/ConstantPropagationOptimizerTest.java
@@ -4,6 +4,7 @@ import static com.google.common.truth.Truth.assertThat;
 
 import org.junit.Test;
 
+import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.plasstech.lang.d2.codegen.il.Nop;
 import com.plasstech.lang.d2.codegen.il.Op;
@@ -11,27 +12,28 @@ import com.plasstech.lang.d2.codegen.il.Transfer;
 
 public class ConstantPropagationOptimizerTest {
   private Optimizer optimizer =
+      new ILOptimizer(ImmutableList.of(new ConstantPropagationOptimizer(2))).setDebugLevel(2);
+
+  private Optimizer optimizers =
       new ILOptimizer(ImmutableList.of(new ConstantPropagationOptimizer(2), new IncDecOptimizer(2)))
           .setDebugLevel(2);
 
   @Test
   public void doubleCopy() {
-    TempLocation t1 = new TempLocation("t1");
-    TempLocation t2 = new TempLocation("t2");
+    TempLocation t1 = new TempLocation("__temp1");
+    TempLocation t2 = new TempLocation("__temp2");
     StackLocation s1 = new StackLocation("s1");
     ImmutableList<Op> program =
         ImmutableList.of(
-            new Transfer(t1, ConstantOperand.ONE),
-            new Transfer(t2, t1),
-            new Transfer(s1, t2));
+            new Transfer(t1, ConstantOperand.ONE), new Transfer(t2, t1), new Transfer(s1, t2));
+    System.out.printf("\nUNOPTIMIZED:\n");
+    System.out.println(Joiner.on("\n").join(program));
+
     program = optimizer.optimize(program);
-    while (optimizer.isChanged()) {
-      program = optimizer.optimize(program);
-    }
 
     assertThat(program.get(0)).isInstanceOf(Nop.class);
     assertThat(program.get(1)).isInstanceOf(Nop.class);
-    Transfer last = (Transfer)program.get(2);
+    Transfer last = (Transfer) program.get(2);
     assertThat(last.source()).isEqualTo(ConstantOperand.ONE);
   }
 
@@ -44,7 +46,7 @@ public class ConstantPropagationOptimizerTest {
             + "  return sum"
             + "}"
             + "println inc(10)",
-        optimizer);
+        optimizers);
   }
 
   @Test
@@ -56,6 +58,6 @@ public class ConstantPropagationOptimizerTest {
             + "  return sum"
             + "}"
             + "println inc(10)",
-        optimizer);
+        optimizers);
   }
 }
