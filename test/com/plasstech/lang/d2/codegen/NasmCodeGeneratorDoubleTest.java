0e5b3ad9a0af0d8d97781b446d58e62741532a4d
David Plass <dplassgit@users.noreply.github.com>
2022-08-02 19:08:41 -0400

    Fixed failing double test, due to rounding. Issue #76

--- test/com/plasstech/lang/d2/codegen/NasmCodeGeneratorDoubleTest.java
+++ test/com/plasstech/lang/d2/codegen/NasmCodeGeneratorDoubleTest.java
@@ -102,14 +102,13 @@ public class NasmCodeGeneratorDoubleTest extends NasmCodeGeneratorTestBase {
             + "e=11.0 "
             + "f=13.0 "
             + "z=0.0 "
-            + " g=a+a*(b+(b+c*(d-(c+d/(-e+(d-e*f)+a)*b)/-c)-d)) print g"
-            + " k=z+4.0/(5.0+(4.0-5.0*-f)) print k"
-            + " k=0.0+-d/(5.0+(4.0-5.0*f)) print k"
-            + " g=a+a*(b+(b+c*(d-(c+d/(e+(d-e*f)))))) print g"
-            + " h=0.0+a+(4.0+3.0*(4.0-(3.0+4.0/(4.0+(5.0-e*6.0))))) print h"
-            + " j=a+a*(b+(b+c*(d-(c+d/(e+(d-e*f+0.0)))))) print j"
-            + " aa=2.0+a*(3.0+(3.0+5.0*(7.0-(5.0+7.0/11.0)+(7.0-11.0*13.0))*2.0)/b) print aa"
-            + "",
+            + " g=a+a*(b+(b+c*(d-(c+d/(-e+(d-e*f)+a)*b)/-c)-d)) println g"
+            + " k=z+4.0/(5.0+(4.0-5.0*-f)) println k"
+            + " k=0.0+-d/(5.0+(4.0-5.0*f)) println k"
+            + " g=a+a*(b+(b+c*(d-(c+d/(e+(d-e*f)))))) println g"
+            + " h=0.0+a+(4.0+3.0*(4.0-(3.0+4.0/(4.0+(5.0-e*6.0))))) println h"
+            + " j=a+a*(b+(b+c*(d-(c+d/(e+(d-e*f+0.0)))))) println j"
+            + " aa=2.0+a*(3.0+(3.0+5.0*(7.0-(5.0+8.0/11.0)+(7.0-11.0*13.0))*2.0)/b) println aa",
         "allOpsGlobal");
   }
 
@@ -135,9 +134,8 @@ public class NasmCodeGeneratorDoubleTest extends NasmCodeGeneratorTestBase {
             + " g=a+a*(b+(b+c*(d-(c+d/(e+(d-e*f)))))) print 'g: ' println g"
             + " h=0.0+a+(4.0+3.0*(4.0-(3.0+4.0/(4.0+(5.0-e*6.0))))) print 'h: ' println h"
             + " j=a+a*(b+(b+c*(d-(c+d/(e+(d-e*f+0.0)))))) print 'j: ' println j"
-            + " aa=2.0+a*(3.0+(3.0+5.0*(7.0-(5.0+7.0/11.0)+(7.0-11.0*13.0))*2.0)/b) print 'aa: ' println aa"
-            + " return aa\n"
-            + " return 3.0 "
+            + " aa=2.0+a*(3.0+(3.0+5.0*(7.0-(5.0+8.0/11.0)+(7.0-11.0*13.0))*2.0)/b) println aa"
+            + " return aa "
             + "} \n"
             + "f2:proc(a:double, b:double):double { return f1(b, a) } "
             + "println f2(1.0, 2.0)",
